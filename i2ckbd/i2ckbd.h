bbf140f13ba7facea6dc977b6c4663a0947e201d
Dashie <dashie@sigpipe.me>
2025-04-02 21:30:17 +0200

    Also reformat that and delete unused file

--- i2ckbd/i2ckbd.h
+++ i2ckbd/i2ckbd.h
@@ -1,15 +1,15 @@
 #ifndef I2C_KEYBOARD_H
 #define I2C_KEYBOARD_H
-#include <pico/stdlib.h>
-#include <pico/platform.h>
 #include <hardware/gpio.h>
 #include <hardware/i2c.h>
+#include <pico/platform.h>
+#include <pico/stdlib.h>
 
 #define I2C_KBD_MOD i2c1
 #define I2C_KBD_SDA 6
 #define I2C_KBD_SCL 7
 
-#define I2C_KBD_SPEED  10000 // if dual i2c, then the speed of keyboard i2c should be 10khz
+#define I2C_KBD_SPEED 10000 // if dual i2c, then the speed of keyboard i2c should be 10khz
 
 #define I2C_KBD_ADDR 0x1F
 
